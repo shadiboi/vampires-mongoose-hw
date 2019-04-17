81f694afd9aa336650178672b9f702eba151ef22
shadiboi <saoutabachi@gmail.com>
2019-04-17 08:07:45 -0600

    Commit 3 - queried for vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -36,21 +36,37 @@ mongoose.connection.on('disconnected', () => {
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
 
-//adds all my vamp data instead of hard insert
-Vampires.collection.insertMany(vampireData,(err, data) => {
-    // if (err){
-    //     console.log(err);
-    // }
-    console.log("added provided vampire data")
-    mongoose.connection.close();
-  });
 
+
+
+
+
+// Note: Remember to close your connection after you add, update, remove from your database
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// INSERT USING MONGOOSE
+// ### Add the vampire data that we gave you
+/////////////////////////////////////////////////
+
+// Vampires.collection.insertMany(vampireData,(err, data) => {
+//     // if (err){
+//     //     console.log(err);
+//     // }
+//     console.log("added provided vampire data")
+//     mongoose.connection.close();
+//   });
+
+// ### Add some new vampire data
+/////////////////////////////////////////////////
+//1. Using the create method, create 4 new vampires with any qualities 
+// that you like two should be male and two should be female.
 
 // Vampires.create({
-//     name:'Shadi',
+//     name:'Shadita',
 //     eye_color: 'brown',
 //     loves: "girls",
-//     victims: 3
+//     victims: 3,
+//     gender: 'm'
 // }, (err,createdVampire)=>{
 //     //this is the callback function
 //     //this is the response from mongodb
@@ -62,21 +78,25 @@ Vampires.collection.insertMany(vampireData,(err, data) => {
 // });
 
 // Vampires.create({
-//     name:'Izzy',
-//     eye_color: 'green',
-//     loves: "boys",
-//     victims: 10
-// },{
 //     name:'Peyton',
 //     eye_color: 'blue',
 //     loves: "boys and girls",
-//     victims: 5
+//     victims: 5,
+//     gender: "f"
+// },
+// {
+//     name:'izzy',
+//     eye_color: 'blue',
+//     loves: ["girls",'hoes','trick','bellywackers'],
+//     victims: 5,
+//     gender: "m"
 // },
 // {
 //     name:'Allison',
 //     eye_color: 'brown',
 //     loves: "girls",
-//     victims: 1
+//     victims: 1,
+//     gender: "f"
 // },
 //  (err,createdVampires)=>{
 //     //this is the callback function
@@ -89,18 +109,72 @@ Vampires.collection.insertMany(vampireData,(err, data) => {
 // });
 
 
-// Note: Remember to close your connection after you add, update, remove from your database
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// INSERT USING MONGOOSE
-// ### Add the vampire data that we gave you
-
-// ### Add some new vampire data
-
-/////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
+
+//1. Find all the vampires that that are females
+
+// Vampires.find({gender:'f'},(err,foundGender) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(foundGender);
+//     }
+// });
+
+//2. have greater than 500 victims
+// Vampires.find({
+//     victims:{
+//     $gt:500
+// }
+// },(err,foundVics) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(foundVics);
+//     }
+// });
+
+//3. have fewer than or equal to 150 victims
+// Vampires.find({
+//         victims:{
+//         $lte:150
+//     }
+//     },(err,foundVics) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(foundVics);
+//         }
+//     });
+
+// 4. have a victim count is not equal to 210234
+// Vampires.find({
+//         victims:{
+//         $ne:210234
+//     }
+//     },(err,foundVics) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(foundVics);
+//         }
+//     });
+
+//5. have greater than 150 AND fewer than 500 victims
+Vampires.find({
+        victims:{
+        $gt:150,
+        $lt: 500
+    }
+    },(err,foundVics) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(foundVics);
+        }
+    });
 
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
